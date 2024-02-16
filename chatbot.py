c9fe5d54ebea5726fa370915fb94a1f20ceef3f7
Srikanth Bachannagari <srikanth.b@ngenux.com>
2024-02-16 19:18:50 +0530

    chatbot TAx

--- chatbot.py
+++ chatbot.py
@@ -3,41 +3,16 @@ import requests
 from datetime import datetime,timedelta,timezone
 from dotenv import load_dotenv
 import os
-import sqlite3
 
 
 load_dotenv()
 
 Query_url = os.getenv("QUERY_URL")
-db = rf"c:\Users\SrikanthBachannagari\Documents\Projects\SQLite\chatbot.db"
-conn = sqlite3.connect(db)
-c = conn.cursor()
-
-
-def log_user_login(name, email,IST_time):
-    print(name, email,IST_time,"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
-    c.execute(f"INSERT INTO user (name, email, logged_in_time) VALUES (?,?,?)",(name,email,IST_time))
-    conn.commit()
-
-def display_login():
-    st.title("Chatbot NPS - Login")
-    name = st.text_input("Enter your name:")
-    email = st.text_input("Enter your email:")
-    UTC_time = datetime.now(timezone.utc)
-    IST_offset = timedelta(hours=5, minutes=30)
-    IST_time = UTC_time + IST_offset
-    if st.button("Log in"):
-        if not name or not email:
-            st.warning("Please enter both name and email.")
-            st.stop()
-        log_user_login(name, email, IST_time)
-        st.success("Logged in successfully! You can now use the chatbot.")
-        st.session_state.logged_in = True
 
 def send_query(query):
     data = {"query": query}
     resp = requests.post(Query_url,json = data)
-    return resp
+    return resp.json().get("body")
 
 
 def display_chats(chats):
@@ -55,12 +30,8 @@ def display_chats(chats):
             st.markdown(f"<span style='font-size: smaller;'>{chat['time']}</span>", unsafe_allow_html=True)
 
 def chatbot():
-    
-    if 'logged_in' not in st.session_state:
-        display_login()
-        st.stop()
     header = st.container()
-    header.title("Chatbot NPS")
+    header.title("Chatbot TAX")
     header.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
 
     st.markdown(
@@ -92,7 +63,7 @@ def chatbot():
         IST_time = UTC_time + IST_offset
         current_time = IST_time.strftime("%H:%M")
         response = send_query(user_query)
-        cleaned_response = response.text.replace("\\n", "<br>").replace("\n", "").replace('"',"")
+        cleaned_response = response.replace("\\n", "<br>").replace("\n", "").replace('"',"")
         chats.history.append({"query": user_query, "response": cleaned_response, "time":current_time})
     
     display_chats(chats.history)
@@ -107,6 +78,5 @@ chatbot()
 
 
   
-
 
 
